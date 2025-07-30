0f5d6b7ffd41dcb78f477128deabfc36aaf9b55a
kritsanaphon kasemsuphat <siraptat43@gmail.com>
2025-07-30 21:13:22 +0700

    เริ่มโปรเจค

---
+++ Banking/banking/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            parrotGradientPanel1 = new ReaLTaiizor.Controls.ParrotGradientPanel();
+            hopeForm1 = new ReaLTaiizor.Forms.HopeForm();
+            parrotWidgetPanel1 = new ReaLTaiizor.Controls.ParrotWidgetPanel();
+            parrotGradientPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // parrotGradientPanel1
+            // 
+            parrotGradientPanel1.BottomLeft = Color.FromArgb(204, 220, 255);
+            parrotGradientPanel1.BottomRight = Color.FromArgb(204, 220, 255);
+            parrotGradientPanel1.CompositingQualityType = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            parrotGradientPanel1.Controls.Add(parrotWidgetPanel1);
+            parrotGradientPanel1.InterpolationType = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
+            parrotGradientPanel1.Location = new Point(-1, 40);
+            parrotGradientPanel1.Name = "parrotGradientPanel1";
+            parrotGradientPanel1.PixelOffsetType = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+            parrotGradientPanel1.PrimerColor = Color.White;
+            parrotGradientPanel1.Size = new Size(1861, 1341);
+            parrotGradientPanel1.SmoothingType = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            parrotGradientPanel1.Style = ReaLTaiizor.Controls.ParrotGradientPanel.GradientStyle.Corners;
+            parrotGradientPanel1.TabIndex = 1;
+            parrotGradientPanel1.TextRenderingType = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            parrotGradientPanel1.TopLeft = Color.White;
+            parrotGradientPanel1.TopRight = Color.White;
+            // 
+            // hopeForm1
+            // 
+            hopeForm1.ControlBoxColorH = Color.FromArgb(228, 231, 237);
+            hopeForm1.ControlBoxColorHC = Color.FromArgb(245, 108, 108);
+            hopeForm1.ControlBoxColorN = Color.White;
+            hopeForm1.Dock = DockStyle.Top;
+            hopeForm1.Font = new Font("Segoe UI", 12F);
+            hopeForm1.ForeColor = Color.FromArgb(242, 246, 252);
+            hopeForm1.Image = (Image)resources.GetObject("hopeForm1.Image");
+            hopeForm1.Location = new Point(0, 0);
+            hopeForm1.Name = "hopeForm1";
+            hopeForm1.Size = new Size(1440, 40);
+            hopeForm1.TabIndex = 2;
+            hopeForm1.Text = "hopeForm1";
+            hopeForm1.ThemeColor = Color.FromArgb(92, 173, 255);
+            // 
+            // parrotWidgetPanel1
+            // 
+            parrotWidgetPanel1.ControlsAsWidgets = false;
+            parrotWidgetPanel1.Location = new Point(366, 42);
+            parrotWidgetPanel1.Name = "parrotWidgetPanel1";
+            parrotWidgetPanel1.Size = new Size(502, 282);
+            parrotWidgetPanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1440, 1020);
+            Controls.Add(hopeForm1);
+            Controls.Add(parrotGradientPanel1);
+            FormBorderStyle = FormBorderStyle.None;
+            MaximumSize = new Size(1920, 1020);
+            MinimumSize = new Size(190, 40);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            parrotGradientPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private ReaLTaiizor.Controls.ParrotGradientPanel parrotGradientPanel1;
+        private ReaLTaiizor.Forms.HopeForm hopeForm1;
+        private ReaLTaiizor.Controls.ParrotWidgetPanel parrotWidgetPanel1;
+    }
+}
